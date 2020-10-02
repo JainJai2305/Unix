08ebeb0bf12a123956e600ff0aec9e08ac018e18
JainJai2305 <53936875+JainJai2305@users.noreply.github.com>
2020-10-02 10:29:50 +0530

    Update read.sh

--- read.sh
+++ read.sh
@@ -3,3 +3,12 @@ read fname
 
 echo "The number of lines are "`wc $fname -l`
 echo "The number of words are "`wc $fname -w`
+echo "Enter file u want to copy contents :\c"
+read file
+cp $flname $file
+echo "Contents in new file"
+cat $file
+echo "Enter new name for the file : ------------------------------"
+read newname
+mv $file $newname
+ls -l $newname
